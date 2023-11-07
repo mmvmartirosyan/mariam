7cee34fe8c1d3fae282f8ae07eb33357a1445607
mmvmartirosyan <147624664+mmvmartirosyan@users.noreply.github.com>
2023-11-07 16:46:37 +0300

    Add files via upload

---
+++ pasp.js
@@ -0,0 +1,13 @@
+const node_for_click = document.getElementById("for_click")
+function find_edit() {
+    document.getElementsByTagName('div')[3].innerText="Martirosyan"
+    document.getElementsByTagName('div')[4].innerText="Mariam"
+    document.getElementsByTagName('div')[5].innerText="Vaganovna"
+    document.getElementsByTagName('div')[8].innerText="Khabarovsk"
+    document.getElementsByTagName('div')[0].innerText= "Ministry of Internal Affairs of Russia for the Khabarovsk Territory"
+    document.getElementsByTagName('div')[6].innerText= "female"
+    
+}
+node_for_click.addEventListener("click",find_edit)
+    
+
